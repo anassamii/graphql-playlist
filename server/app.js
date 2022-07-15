58db197423a6909e7718bb2865cbdc627f5e095d
Anas SAMII <samiianas000@gmail.com>
2022-07-15 18:03:58 +0100

    anas

--- server/app.js
+++ server/app.js
@@ -11,7 +11,7 @@ app.use(cors());
 
 // connect to mlab database
 // make sure to replace my db string & creds with your own
-mongoose.connect('mongodb://ninja:test@ds161148.mlab.com:61148/graphql-ninja')
+mongoose.connect('mongodb://anassamii:anas123@ac-jbqn1pq-shard-00-00.fudzgu7.mongodb.net:27017,ac-jbqn1pq-shard-00-01.fudzgu7.mongodb.net:27017,ac-jbqn1pq-shard-00-02.fudzgu7.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-pfvpcl-shard-0&authSource=admin&retryWrites=true&w=majority')
 mongoose.connection.once('open', () => {
     console.log('conneted to database');
 });
